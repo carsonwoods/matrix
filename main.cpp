8b9bcdd72901b4a138ea7851ce3f2b30d32e10a0
Carson Woods <cwoods289@gmail.com>
2018-05-30 21:03:24 -0400

    Change test case to valid

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ int main(int argc, char *argv[]){
 	  oDM2(2,3), // Call Overloaded Constructor (size_t,size_t)
 	  oDM3{{1,2,3},{4,5,6}},
 	  oDM4{{1,1},{1,1},{1,1}},
-		 oDM5(oDM2*oDM3);
+		 oDM5(oDM3*oDM4);
 		 cout << "oDM1: " << endl << oDM1 << endl
 			  << "oDM2: " << endl << oDM2 << endl
 			  << "oDM3: " << endl << oDM3 << endl
 			  << "oDM4: " << endl << oDM4 << endl
 			  << "oDM5: " << endl << oDM5 << endl;
-   
+
 		 // oDM2(2,3); // Call Overloaded Constructor (size_t,size_t)
-		 ; 
+		 
 		 //*pDM3;    // Call Default Constructor at Runtime
 		 //*pDM4(3,4),   //
 		 //*pDM1(&oDM1),
