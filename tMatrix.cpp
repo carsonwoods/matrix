30e06702738763f0784687f55f42b14253894061
Carson Woods <cwoods289@gmail.com>
2018-05-31 20:27:53 -0400

    Implement overloaded [] operator

--- tMatrix.cpp
+++ tMatrix.cpp
@@ -1,70 +1,88 @@
 #include <iostream> // cout
-
 #include "tMatrix.h"
 
 #define DEBUG
 
+//overloads operator for << of tRow (typedef for vector<int>)
 ostream& operator<<(ostream& os, const tRow& V){
-   os << "[";
-   for (size_t iElt(0); iElt < V.size(); ++iElt) {
-	  os << V[iElt];
-	  if (iElt != V.size() - 1)
-		 os << ", ";
-   }
-   os << "]";
-   return os;
+    os << "[";
+    for (size_t iElt(0); iElt < V.size(); ++iElt) {
+	    os << V[iElt];
+	    if (iElt != V.size() - 1)
+		   os << ", ";
+     }
+     os << "]";
+     return os;
 }
-ostream& operator<<(ostream& os, const tMatrix& M){
-   os << "{";
-   for (size_t iRow(0); iRow < M.Data.size(); ++iRow) {
-	  os << " ";
-	  os << M.Data[iRow];
-	  if (iRow != M.Data.size() - 1)
-		 os << "\n ";
-   }
-   os << " }";
-   return os;
-}
-const tRC tMatrix::size() const {
-   return {Data.size(),Data[0].size()};
-}
-void tMatrix::Zero(size_t pNumRows,size_t pNumCols){
-#ifdef xDEBUG
-   printf("Zero(%ld,%ld): ENTER.\n",pNumRows,pNumCols);
-#endif
-   Data.clear();
-   tRow rRow(pNumCols,0) ;
-   for (size_t iRow(0); iRow < pNumRows; iRow++) {
-	  Data.push_back(rRow);
-   }
-#ifdef xDEBUG
-   printf("Zero(): Data.size(%ld).\n",Data.size());
-   printf("Zero(%ld,%ld): RETURN.\n",pNumRows,pNumCols);
-#endif
-}
-tMatrix::tMatrix(){
-   cout << "tMatrix(default): RETURN.\n";
-}
-tMatrix::tMatrix(size_t pNumRows, size_t pNumCols) {
-#ifdef DEBUG
-   printf("tMatrix(%ld,%ld): ENTER.\n",pNumRows,pNumCols);
-#endif
-   Zero(pNumRows,pNumCols); // Make a NumRows X NumCols Zero Matrix
-#ifdef xDEBUG
-   printf("tMatrix(%ld,%ld): RETURN.\n",pNumRows,pNumCols);
-#endif
 
+//overloads operator for << of tMatrix
+ostream& operator<<(ostream& os, const tMatrix& M){
+    os << "{";
+    for (size_t iRow(0); iRow < M.Data.size(); ++iRow) {
+	    os << " ";
+	    os << M.Data[iRow];
+	    if (iRow != M.Data.size() - 1)
+		   os << "\n ";
+     }
+     os << " }";
+     return os;
 }
+
+const tRC tMatrix::size() const {
+    return {Data.size(),Data[0].size()};
+}
+
+void tMatrix::Zero(size_t pNumRows,size_t pNumCols){
+    #ifdef xDEBUG
+        printf("Zero(%ld,%ld): ENTER.\n",pNumRows,pNumCols);
+    #endif
+
+    Data.clear();
+    tRow rRow(pNumCols,0) ;
+    for (size_t iRow(0); iRow < pNumRows; iRow++) {
+	    Data.push_back(rRow);
+
+    }
+    #ifdef xDEBUG
+        printf("Zero(): Data.size(%ld).\n",Data.size());
+        printf("Zero(%ld,%ld): RETURN.\n",pNumRows,pNumCols);
+    #endif
+}
+
+
+tMatrix::tMatrix(){
+    cout << "tMatrix(default): RETURN.\n";
+}
+
+
+tMatrix::tMatrix(size_t pNumRows, size_t pNumCols) {
+    #ifdef DEBUG
+        printf("tMatrix(%ld,%ld): ENTER.\n",pNumRows,pNumCols);
+    #endif
+
+        Zero(pNumRows,pNumCols); // Make a NumRows X NumCols Zero Matrix
+
+    #ifdef xDEBUG
+        printf("tMatrix(%ld,%ld): RETURN.\n",pNumRows,pNumCols);
+    #endif
+}
+
+
 tMatrix::tMatrix(initializer_list<tRow> il)
-   : Data(il) {
-#ifdef DEBUG
-   printf ("tMatrix(IL): ENTER.\n");
-#endif
-   // TODO: Implement for Learning Purposes
-#ifdef DEBUG
-   printf ("tMatrix(IL): RETURN.\n");
-#endif
+    : Data(il) {
+
+    #ifdef DEBUG
+    printf ("tMatrix(IL): ENTER.\n");
+    #endif
+
+    // TODO: Implement for Learning Purposes
+
+    #ifdef DEBUG
+    printf ("tMatrix(IL): RETURN.\n");
+    #endif
 }
+
+
 tMatrix tMatrix::operator*(const tMatrix& RHS) const  {
     const tMatrix LHS(*this);
 
@@ -93,4 +111,9 @@ tMatrix tMatrix::operator*(const tMatrix& RHS) const  {
 		}
 	}
 	return RET;
+}
+
+
+tRow tMatrix::operator[](int iElt) {
+    return Data[iElt];
 }
--- tMatrix.h
+++ tMatrix.h
@@ -9,14 +9,15 @@ typedef vector<int> tRow;
 
 struct tMatrix {
    vector< tRow > Data;
-   
+
    void Zero(size_t pNumRows,size_t pNumCols);
    const tRC size() const; // TODO: WHY??
    tMatrix();                                // Default Constructor
    tMatrix(size_t pNumRows, size_t pNumCols);
    tMatrix(initializer_list<tRow> il);
-   
-   tMatrix operator*(const tMatrix& RHS) const ;
+
+   tMatrix operator*(const tMatrix& RHS) const;
+   tRow    operator[](int iElt);
 };
 
 ostream& operator<<(ostream& os, const    tRow& V);
