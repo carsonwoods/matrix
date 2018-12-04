5387673d186ed51fb07de61c0945a10ee1cfadec
Carson Woods <cwoods289@gmail.com>
2018-12-04 16:53:08 -0500

    Start test case

--- DenseMatrixGEMM.cu
+++ DenseMatrixGEMM.cu
@@ -19,13 +19,19 @@ void FGEMM(int n, DenseMatrix<float> *a, DenseMatrix<float> *b) {
 
 
 int main() {
+
+	int N = 6;
     
     DenseMatrix<float>
         oDM1{{1,2,3},{4,5,6}},
-        oDM2{{4,9},{12347,835},{91,7532}},
-        oDM3(oDM1*oDM2),
-        oDM4(oDM3);
+        oDM2{{4,9},{12347,835},{91,7532}};
 
+
+    cudaMalloc(&oDM1, sizeof(DenseMatrix<float>))
+    cudaMalloc(&oDM2, sizeof(DenseMatrix<float>))
+
+
+    FGEMM<<<1, 1>>>(N, oDM1, oDM2)
 
     cout << "Hello World" << endl;
 
